db26b276a797dabdc98a80ec5cb727c0bc7122db
Alvin <actchua@periapsys.com>
2026-05-16 13:30:19 +0800

    Captcha

--- SSO.Business/Accounts/Commands/PasswordRecoveryCommand.cs
+++ SSO.Business/Accounts/Commands/PasswordRecoveryCommand.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MediatR;
+using SSO.Business.Captchas;
 
 namespace SSO.Business.Accounts.Commands
 {
-    internal class PasswordRecoveryCommand
+    public class PasswordRecoveryCommand : IRequest<Unit>
     {
+        public CaptchaRequest Captcha { get; set; }
+        public string Email { get; set; }
+        public Guid? RealmId { get; set; }
     }
 }
