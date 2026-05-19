126c658259f0e565ddbcce228a66f0d179f4843c
Alvin <actchua@periapsys.com>
2026-05-19 20:49:44 +0800

    Postgres

--- SSO.Infrastructure.Db.Postgres/Migrations/20260519124556_RealmMailerSettingsTable.Designer.cs
+++ SSO.Infrastructure.Db.Postgres/Migrations/20260519124556_RealmMailerSettingsTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SSO.Infrastructure.Db.Postgres;
@@ -11,9 +12,11 @@ using SSO.Infrastructure.Db.Postgres;
 namespace SSO.Infrastructure.Db.Postgres.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260519124556_RealmMailerSettingsTable")]
+    partial class RealmMailerSettingsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
